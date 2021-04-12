9db1084dd1cf142d7c8a583149f286316608c25e
leastbad <hello@leastbad.com>
2021-04-12 03:22:23 -0400

    v1.0.2

--- lib/chrono_trigger/event.rb
+++ lib/chrono_trigger/event.rb
@@ -15,7 +15,6 @@ module ChronoTrigger
           after: @after
         )
         new(options, args)
-        id
       end
 
       def scope(value)
@@ -107,6 +106,10 @@ module ChronoTrigger
       raise(LostInTimeError, "Argument must be of type ActiveSupport::TimeWithZone eg. right_now + 1.second") unless time_with_zone.is_a?(ActiveSupport::TimeWithZone)
       raise(TodayIsTomorrowsYesterday, "Time must be in the future eg. 1.second.from_now") unless time_with_zone.future?
       Time.zone.today + time_with_zone.hour.hours + time_with_zone.min.minutes + time_with_zone.sec.seconds
+    end
+
+    def to_s
+      inspect
     end
 
     private
--- lib/chrono_trigger/worker.rb
+++ lib/chrono_trigger/worker.rb
@@ -4,7 +4,7 @@ module ChronoTrigger
   class Worker < Concurrent::Actor::RestartingContext
     def on_message(event)
       Rails.logger.debug "ChronoTrigger: #{event.inspect}"
-      event.perform(*event.args) if event.at.nil? || (event.before && event.at < event.before)
+      event.perform(*event.args) if event.at.nil? || (event.at && event.before.nil?) || (event.before && event.at < event.before)
     end
   end
 end
